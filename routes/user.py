3f654308d64e0d742439ae09189893d2cf23e861
Fernando Zurita <ferzuritag@hotmail.com>
2024-03-31 17:03:08 -0600

    add: api key validation

--- routes/user.py
+++ routes/user.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter, Request
+from typing import Annotated
+from fastapi import APIRouter, Request, Header
 
 from methods.get_user_information import get_user_information
 from methods.sign_in_user import sign_in_user
 from methods.delete_user import delete_user
 from methods.confirm_user_account import confirm_user_account
+from security.checkAPIKey import check_api_key
 
 user = APIRouter()
 
 @user.get('/users/{user_email}')
-def getUserInformation(user_email):
+def getUserInformation(user_email, api_key: Annotated[str | None, Header()] = None):
+    check_api_key(api_key)
     return get_user_information(user_email)
 
 @user.get('/users/confirm-account/{user_email}/{account_confirmation_token}')
@@ -22,4 +25,3 @@ async def signInUser(request: Request):
 @user.delete('/users/{user_email}')
 async def deleteUser(user_email):
     return delete_user(user_email)
-
